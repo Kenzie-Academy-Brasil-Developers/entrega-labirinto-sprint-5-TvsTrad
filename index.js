f1fbc4cca830f36069265da21db6aeec21b957b1
Thiago Trad <thiago_trad@hotmail.com>
2021-08-06 23:48:53 -0300

    added several imgs , fixed mazeGenerator funcition implementing validade and move functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let container = document.getElementById('container')
+let container = document.getElementById('container');
+//
 
-// 
 const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
     "W   W     W     W W W",
@@ -19,16 +19,74 @@ const map = [
     "WWWWWWWWWWWWWWWWWWWWW",
 ];
 
-
-
 function mazeGenerator() {
     for ( let i = 0 ; i < map.length ; i++) {
         let line = map[i].split('')
+        let lineCont = document.createElement('div')
+        lineCont.setAttribute('id', `line${i}`);
+        lineCont.setAttribute('class' , 'lines');
+        container.appendChild(lineCont)
         for( j = 0 ; j < line.length; j++) {
-            let cell = document.createElement('div');
-            cell.setAttribute('id', `cell${i} ${j}`)
+            let cell = line[j]
+            let cellCont = document.createElement('div');
+            cellCont.setAttribute('id', `${i} ${j}`);
+            cellCont.setAttribute('class', 'cells');
+            if (cell === "W") {
+                cellCont.classList.add("wall")
+            }
+            if (cell === " ") {
+                cellCont.classList.add('path')
+            }
+            if (cell === "S") {
+                cellCont.classList.add('duck')
+            }
+            if (cell === "F") {
+                cellCont.classList.add('exit')
+            }
+            lineCont.appendChild(cellCont)
         }       
     }
 }
+mazeGenerator();
 
-mazeGenerator()
+function movement(){
+    document.addEventListener('keydown', (event) => {
+        const keyName = event.key;
+        console.log(keyName)
+        if (keyName === "ArrowDown") {
+        
+        }
+        if (keyName === 'ArrowUp') {
+        
+        }
+        if (keyName === "ArrowLeft") {
+        
+        }
+        if (keyName === "ArrowRight") {
+        
+        }
+        
+  })
+};
+
+let starterCell = document.getElementsByClassName("duck")[0]
+let currentCell = starterCell.id;
+console.log(currentCell)
+function validate() {
+    document.addEventListener('keydown', (event) => {
+        const keyName = event.key;
+        console.log(keyName)
+        if (keyName === "ArrowDown") {
+        
+        }
+        if (keyName === 'ArrowUp') {
+        
+        }
+        if (keyName === "ArrowLeft") {
+        
+        }
+        if (keyName === "ArrowRight") {
+        
+        }
+    })
+}
