38081d9da97a08b02045559a480ac787e4a39542
Thiago Trad <thiago_trad@hotmail.com>
2021-08-07 01:18:44 -0300

    indo dormir

--- index.js
+++ index.js
@@ -20,16 +20,16 @@ const map = [
 ];
 
 function mazeGenerator() {
-    for ( let i = 0 ; i < map.length ; i++) {
-        let line = map[i].split('')
+    for ( let i = 10 ; i < (map.length +10)  ; i++) {
+        let line = map[ i -10 ].split('')
         let lineCont = document.createElement('div')
         lineCont.setAttribute('id', `line${i}`);
         lineCont.setAttribute('class' , 'lines');
         container.appendChild(lineCont)
-        for( j = 0 ; j < line.length; j++) {
-            let cell = line[j]
+        for( j = 10 ; j < line.length + 10; j++) {
+            let cell = line[ j - 10]
             let cellCont = document.createElement('div');
-            cellCont.setAttribute('id', `${i} ${j}`);
+            cellCont.setAttribute('id', `L${i}${j}`);
             cellCont.setAttribute('class', 'cells');
             if (cell === "W") {
                 cellCont.classList.add("wall")
@@ -38,9 +38,11 @@ function mazeGenerator() {
                 cellCont.classList.add('path')
             }
             if (cell === "S") {
+                cellCont.classList.add('path')
                 cellCont.classList.add('duck')
             }
             if (cell === "F") {
+                cellCont.classList.add('path')
                 cellCont.classList.add('exit')
             }
             lineCont.appendChild(cellCont)
@@ -49,33 +51,60 @@ function mazeGenerator() {
 }
 mazeGenerator();
 
+const starterCell = document.getElementsByClassName("duck")[0]
+let currentCellId = starterCell.id;
+let currentCell = starterCell
+let moveIndex = Number(currentCellId.replace(/L/g, ''))
+let nextCell = document.getElementById(moveIndex)
+// usar o ID da currentCellId para definir o ID da nextCell .
+
+
+
 function movement(){
     document.addEventListener('keydown', (event) => {
         const keyName = event.key;
-        console.log(keyName)
         if (keyName === "ArrowDown") {
-        
+            moveIndex += 100
+            moveIndex = `L${moveIndex}`
+            nextCell = document.getElementById(moveIndex)
+            currentCell.classList.remove("duck")
+            nextCell.classList.add("duck")
+            currentCell = nextCell
+            return currentCell            
         }
         if (keyName === 'ArrowUp') {
-        
+            moveIndex -= 100
+            moveIndex = `L${moveIndex}`
+            nextCell = document.getElementById(moveIndex)
+            currentCell.classList.remove("duck")
+            nextCell.classList.add("duck")
+            currentCell = nextCell.id
+            
         }
         if (keyName === "ArrowLeft") {
-        
+            moveIndex -= 1
+            moveIndex = `L${moveIndex}`
+            nextCell = document.getElementById(moveIndex)
+            currentCell.classList.remove("duck")
+            nextCell.classList.add("duck")
+            currentCell = nextCell.id
         }
         if (keyName === "ArrowRight") {
-        
+            moveIndex += 1
+            moveIndex = `L${moveIndex}`
+            nextCell = document.getElementById(moveIndex)
+            currentCell.classList.remove("duck")
+            nextCell.classList.add("duck")
+            currentCell = nextCell.id
         }
-        
   })
 };
 
-let starterCell = document.getElementsByClassName("duck")[0]
-let currentCell = starterCell.id;
-console.log(currentCell)
+movement()
+
 function validate() {
     document.addEventListener('keydown', (event) => {
         const keyName = event.key;
-        console.log(keyName)
         if (keyName === "ArrowDown") {
         
         }
